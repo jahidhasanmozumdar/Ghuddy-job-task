72a826b07a5584a3af087816d595b49d85e0fdc6
jahidhasanmozumdar <jahidhasanmozumdar9@gmail.com>
2023-08-08 15:32:45 +0600

    project setup And tailwind css installed

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './Component/Header'
+
+function App() {
+
+  return (
+    <Header></Header>
+  )
+}
+
+export default App
